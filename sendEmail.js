faca002cbd80e549dd9500e1c1aa6541ee8c1529
Prayag Sahu <sahuprayag229@gmail.com>
2026-01-03 20:46:00 +0530

    add

--- sendEmail.js
+++ sendEmail.js
@@ -1,20 +1,58 @@
+// const nodemailer = require("nodemailer");
+
+// const sendEmail = async (to, subject, text) => {
+//     const transporter = nodemailer.createTransport({
+//         service: "Gmail",
+//         auth: {
+//             user: 'sahuprayag145@gmail.com',
+//             pass: 'hhxx dbcp havm tiox', // App password
+//         },
+//     });
+
+//     await transporter.sendMail({
+//         from: 'sahuprayag145@gmail.com',
+//         to,
+//         subject,
+//         text,
+//     });
+// };
+
+// module.exports = { sendEmail };
+
+
+
+
+
+
 const nodemailer = require("nodemailer");
 
 const sendEmail = async (to, subject, text) => {
+    console.log("📨 Initializing mail transporter...");
+
     const transporter = nodemailer.createTransport({
         service: "Gmail",
         auth: {
             user: 'sahuprayag145@gmail.com',
-            pass: 'hhxx dbcp havm tiox', // App password
+                        pass: 'hhxx dbcp havm tiox', // App password
         },
+        connectionTimeout: 10000,
+        greetingTimeout: 10000,
+        socketTimeout: 10000
     });
 
+    console.log("🔍 Verifying transporter...");
+    await transporter.verify();
+    console.log("✅ Transporter verified");
+
+    console.log("📤 Sending email...");
     await transporter.sendMail({
         from: 'sahuprayag145@gmail.com',
         to,
         subject,
-        text,
+        text
     });
+
+    console.log("📬 Email sendMail resolved");
 };
 
 module.exports = { sendEmail };
