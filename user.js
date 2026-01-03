20d73b95ad7735fe4b7f91981a3448d9bae2abec
Prayag Sahu <sahuprayag229@gmail.com>
2026-01-03 19:57:25 +0530

    add

--- user.js
+++ user.js
@@ -1,42 +1,25 @@
-// let mongoose=     require('mongoose')
-//  let userSchema=   mongoose.Schema({
-//     userName:{
-//         type:String,
-//         required:true
-//     },
-//     email:{
-//         type:String,
-//         required:true
+let mongoose=     require('mongoose')
+ let userSchema=   mongoose.Schema({
+    userName:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
 
-//     },passWord:{
-//         type:String,
+    },passWord:{
+        type:String,
 
-//     },
-//     role:{
-//         type:String,
-//         enum:['admin','student','instructor'],
-//         default:'student'
-//     },
-//     resetToken: String,
-//   resetTokenExpiry: Date,
-// })
-
-// let User=  mongoose.model('user',userSchema)
-// module.exports=User
-
-
-
-
-// models/User.js (Mongoose schema)
-const mongoose = require('mongoose');
-
-const userSchema = new mongoose.Schema({
-    userName: String,
-    email: String,
-    passWord: String,
-    role: { type: String, default: 'student' },  // default role = student:contentReference[oaicite:6]{index=6}
+    },
+    role:{
+        type:String,
+        enum:['admin','student','instructor'],
+        default:'student'
+    },
     resetToken: String,
-    resetTokenExpiry: Date
-});
+  resetTokenExpiry: Date,
+})
 
-module.exports = mongoose.model('User', userSchema);
+let User=  mongoose.model('user',userSchema)
+module.exports=User
